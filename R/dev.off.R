0123a4a456aba95465e861be5a33fdcb5ee71a1e
leeper <thosjleeper@gmail.com>
2014-07-17 11:58:51 +0200

    move imgur_off code

--- R/dev.off.R
+++
@@ -1,7 +0,0 @@
-imgur_off <- function(obj, justLink=TRUE) {
-    if(!inherits(obj, 'imgur_device'))
-        stop("'obj' is not of class 'imgur_device'")
-    dev.off(obj$current)
-    tmp <- imguRupload(obj$filename, obj$title, obj$caption, obj$name)
-    return(temp)
-}
--- R/imguR.R
+++ R/imguR.R
@@ -1,4 +1,4 @@
-imgur <- function(device, title = NULL, caption = NULL, name = NULL, ...) {
+imgur <- function(device = png, title = NULL, caption = NULL, name = NULL, ...) {
     tmpfile <- tempfile()
     do.call(device, c(file = tmpfile, list(...)))
     structure(list(filename = tmpfile,
@@ -6,4 +6,12 @@ imgur <- function(device, title = NULL, caption = NULL, name = NULL, ...) {
                    title = title, 
                    caption = caption, 
                    name = name), class = 'imgur-device')
+}
+
+imgur_off <- function(obj, justLink=TRUE) {
+    if(!inherits(obj, 'imgur_device'))
+        stop("'obj' is not of class 'imgur_device'")
+    dev.off(obj$current)
+    tmp <- imguRupload(obj$filename, obj$title, obj$caption, obj$name)
+    return(temp)
 }
