8b07bfe187e6b8fa34870a8146e6d595975a83b2
leeper <thosjleeper@gmail.com>
2014-07-17 14:20:47 +0200

    add basic HTTP functions

---
+++ R/http.r
@@ -0,0 +1,83 @@
+imgurGET <-
+function(endpoint, 
+         base_url = "https://api.imgur.com/3/", 
+         key = "1babd0decbb90f2", 
+         token = NULL,
+         ...){
+    if(!is.null(token)){
+        out <- GET(paste0(base_url, endpoint), 
+                   config(httpheader = c(Authorization = paste('Bearer', token))),
+                   ...)
+    } else if(!is.null(key)) {
+        out <- GET(paste0(base_url, endpoint), 
+                   config(httpheader = c(Authorization = paste('Client-ID', key))),
+                   ...)
+    } else {
+        stop("Must specify an API key or OAuth2.0 Access Token.")
+    }
+    stop_for_status(out)
+    return(out)
+}
+
+imgurPOST <-
+function(endpoint, 
+         base_url = "https://api.imgur.com/3/", 
+         key = "1babd0decbb90f2", 
+         token = NULL,
+         ...){
+    if(!is.null(token)){
+        out <- POST(paste0(base_url, endpoint), 
+                    config(httpheader = c(Authorization = paste('Bearer', token))),
+                    ...)
+    } else if(!is.null(key)) {
+        out <- POST(paste0(base_url, endpoint), 
+                    config(httpheader = c(Authorization = paste('Client-ID', key))),
+                    ...)
+    } else {
+        stop("Must specify an API key or OAuth2.0 Access Token.")
+    }
+    stop_for_status(out)
+    return(out)
+}
+
+imgurPUT <-
+function(endpoint, 
+         base_url = "https://api.imgur.com/3/", 
+         key = "1babd0decbb90f2", 
+         token = NULL,
+         ...){
+    if(!is.null(token)){
+        out <- PUT(paste0(base_url, endpoint), 
+                   config(httpheader = c(Authorization = paste('Bearer', token))),
+                   ...)
+    } else if(!is.null(key)) {
+        out <- PUT(paste0(base_url, endpoint), 
+                   config(httpheader = c(Authorization = paste('Client-ID', key))),
+                   ...)
+    } else {
+        stop("Must specify an API key or OAuth2.0 Access Token.")
+    }
+    stop_for_status(out)
+    return(out)
+}
+
+imgurDELETE <-
+function(endpoint, 
+         base_url = "https://api.imgur.com/3/", 
+         key = "1babd0decbb90f2", 
+         token = NULL,
+         ...){
+    if(!is.null(token)){
+        out <- DELETE(paste0(base_url, endpoint), 
+                      config(httpheader = c(Authorization = paste('Bearer', token))),
+                      ...)
+    } else if(!is.null(key)) {
+        out <- DELETE(paste0(base_url, endpoint), 
+                      config(httpheader = c(Authorization = paste('Client-ID', key))),
+                      ...)
+    } else {
+        stop("Must specify an API key or OAuth2.0 Access Token.")
+    }
+    stop_for_status(out)
+    return(out)
+}
