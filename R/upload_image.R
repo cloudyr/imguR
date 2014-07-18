ffe3cd8a3277ecbf7b6ca42f53eb2a3e2206704b
leeper <thosjleeper@gmail.com>
2014-07-18 18:59:03 +0200

    rename upload file

--- R/upload_image.R
+++ R/upload_image.R
@@ -9,9 +9,17 @@ function(file,
          ...) {
     if(!file.exists(file))
         stop("File not found!")
-    b <- list(image = fileUpload(file),
-              title = title, 
-              description = description)
+    b <- list(image = fileUpload(file))
+    if(!is.null(title))
+        b$title <- title
+    if(!is.null(description))
+        b$description <- description
+    if(!is.null(album))
+        b$album <- album
+    if(!is.null(name))
+        b$name <- name
+    if(!is.null(type))
+        b$type <- type
     out <- imgurPOST('image', body = b, ...)
     structure(out, class = 'imgur_image')
 }
